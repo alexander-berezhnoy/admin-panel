deedebc1f1ab122e8561ea0a1ac2a380c52cbeae
Oleksandr Berezhnyi <berrezhnoy@gmail.com>
2025-10-25 01:14:45 +0300

    Create general layout and add themes

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Button variant="destructive" size="lg">
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default Homepage;
