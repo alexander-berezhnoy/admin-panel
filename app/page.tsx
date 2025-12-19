c572ad5dfee42c2010dce1937ebff1a01e6bc38b
Oleksandr Berezhnyi <berrezhnoy@gmail.com>
2025-12-19 08:17:32 +0200

    Add charts and cards proxy

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import AppBarChart from "@/components/AppBarChart";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppPieChart from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
 
 const Homepage = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Button variant="destructive" size="lg">
-        Click me
-      </Button>
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Latest Transactions" />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Popular Content" />
+      </div>
     </div>
   );
 };
